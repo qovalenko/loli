83b0ff247e29a2fd48e541ced3b751fcb1382c7c
Nikita Kalanakov <39966671+txlyre@users.noreply.github.com>
2019-01-11 11:13:03 +0300

    Add builtin sys.exit()

--- loli_pkg_sys.c
+++ loli_pkg_sys.c
@@ -8,18 +8,21 @@
 
 const char *loli_sys_info_table[] = {
     "\0\0"
+    ,"F\0exit\0(*Integer)"
     ,"F\0getenv\0(String): Option[String]"
     ,"F\0recursion_limit\0: Integer"
     ,"F\0set_recursion_limit\0(Integer)"
     ,"R\0argv\0List[String]"
     ,"Z"
 };
+void loli_sys__exit(loli_state *s);
 void loli_sys__getenv(loli_state *);
 void loli_sys__recursion_limit(loli_state *);
 void loli_sys__set_recursion_limit(loli_state *);
 void loli_sys_var_argv(loli_state *);
 loli_call_entry_func loli_sys_call_table[] = {
     NULL,
+    loli_sys__exit,
     loli_sys__getenv,
     loli_sys__recursion_limit,
     loli_sys__set_recursion_limit,
@@ -74,3 +77,14 @@ void loli_sys__set_recursion_limit(loli_state *s)
     s->depth_max = (int32_t)limit;
     loli_return_unit(s);
 }
+
+void loli_sys__exit(loli_state *s)
+{
+    int64_t exit_code = 0;
+    if (loli_arg_count(s) == 1)
+        exit_code = loli_arg_integer(s, 0);
+    
+    exit(exit_code);    
+    loli_return_unit(s); // should never be reached, but who knows...
+}
+
